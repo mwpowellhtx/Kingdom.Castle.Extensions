781a494e18388322451f56cf00638db56b602f58
Michael W Powell <mwpowellhtx@gmail.com>
2016-11-25 18:15:02 -0500

    updated deployment

--- src/tools/NuGet/deploy.cmd
+++ src/tools/NuGet/deploy.cmd
@@ -7,12 +7,13 @@ set nupkg_files=*.nupkg
 
 pushd ..\..
 
-if not exist Kindgom.AspNet.WebApi.Castle.Windsor\%nupkg_files% goto end
-
 if not exist %local_packages_dir% mkdir %local_packages_dir%
 
 echo Copying files...
-xcopy "Kindgom.AspNet.WebApi.Castle.Windsor\%nupkg_files%" "%local_packages_dir%\%nupkg_files%" /Y
+
+for /R %%f in (Kindgom.AspNet.WebApi.Castle.Windsor\%nupkg_files%) do (
+    xcopy "%%f" %local_packages_dir% /Y
+)
 
 :end
 
