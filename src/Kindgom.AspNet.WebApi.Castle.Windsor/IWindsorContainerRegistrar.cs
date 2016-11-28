81a811ea62364c7eb9f00fa72084625b8d7723ee
Michael W Powell <mwpowellhtx@gmail.com>
2016-11-28 11:46:37 -0500

    updated the asp dot net web api dependency resolver configuration sequence for use with castle windsor

---
+++ src/Kindgom.AspNet.WebApi.Castle.Windsor/IWindsorContainerRegistrar.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+
+namespace Kingdom.Castle.Windsor.Web.Http
+{
+    using Dependencies;
+    using global::Castle.Windsor;
+
+    /// <summary>
+    /// <see cref="IWindsorContainer"/> registrar interface.
+    /// </summary>
+    public interface IWindsorContainerRegistrar
+    {
+        /// <summary>
+        /// Configures the <see cref="HttpConfiguration.DependencyResolver"/> to use the
+        /// <see cref="IWindsorDependencyResolver"/>.
+        /// </summary>
+        /// <param name="getDependencyResolver"></param>
+        void ConfigureDependencyResolver(WindsorDependencyResolverFactory getDependencyResolver = null);
+    }
+
+    /// <summary>
+    /// <see cref="IWindsorDependencyResolver"/> factory based on the <paramref name="container"/>.
+    /// </summary>
+    /// <param name="container"></param>
+    /// <returns></returns>
+    public delegate IWindsorDependencyResolver WindsorDependencyResolverFactory(IWindsorContainer container);
+}
---
+++ src/Kindgom.AspNet.WebApi.Castle.Windsor/WindsorExtensionMethods.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Http;
+
+namespace Kingdom.Castle.Windsor.Web.Http
+{
+    using Owin;
+    using global::Castle.Windsor;
+
+    /// <summary>
+    /// Provides the hooks for <see cref="IWindsorContainer"/> integration with ASP.NET Web API.
+    /// </summary>
+    public static class WindsorExtensionMethods
+    {
+        private static IWindsorContainer GetDefaultWindsorContainer(HttpConfiguration config)
+        {
+            return new WindsorContainer();
+        }
+
+        /// <summary>
+        /// Uses <see cref="IWindsorContainer"/> with the <paramref name="config"/>.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="config"></param>
+        /// <param name="getContainer"></param>
+        /// <returns></returns>
+        public static IWindsorContainerRegistrar UseCastleWindsor(this IAppBuilder app,
+            HttpConfiguration config, WindsorContainerFactory getContainer = null)
+        {
+            getContainer = getContainer ?? GetDefaultWindsorContainer;
+            return new WindsorContainerRegistrar(config, getContainer(config));
+        }
+    }
+
+    /// <summary>
+    /// <see cref="IWindsorContainer"/> factory based on the <paramref name="config"/>.
+    /// </summary>
+    /// <param name="config"></param>
+    /// <returns></returns>
+    public delegate IWindsorContainer WindsorContainerFactory(HttpConfiguration config);
+}
