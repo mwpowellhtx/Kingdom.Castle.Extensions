cd26c6b7a953518ba4c114fad4b3fadfa632d015
Michael W Powell <mwpowellhtx@gmail.com>
2017-01-03 17:30:28 -0500

    added asp dot net mvc website for test purposes would be better if we could capture end to end testing but this will have to do for now

---
+++ src/Kingdom.Web.Mvc.Castle.Windsor.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kingdom.Web.Mvc.Castle.Windsor.Website.MvcApplication" Language="C#" %>
---
+++ src/Kingdom.Web.Mvc.Castle.Windsor.Website/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Kingdom.Web.Mvc.Castle.Windsor.Website
+{
+    using Controllers;
+    using global::Castle.Windsor;
+
+    public class MvcApplication : HttpApplication
+    {
+        private IWindsorContainer Container { get; }
+
+        public MvcApplication()
+        {
+            Container = new WindsorContainer();
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Assumes filters, routes, bundles, etc, have all been configured.
+            Container.InstallMvcServices<HomeController>()
+                .UseDependencyResolver()
+                ;
+        }
+    }
+}
